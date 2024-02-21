70776dee0eef7084b595fce2b7745ec8f25df08d
OpenVAET <openvaet@protonmail.com>
2024-02-21 16:59:32 +0100

    NZ Data - v 1.23 - Stillbirths comparison OIA vs Ref.

--- compare_oia_to_ref_deaths.pl
+++ compare_oia_to_ref_deaths.pl
@@ -20,17 +20,25 @@ use DateTime;
 use FindBin;
 use lib "$FindBin::Bin/../../lib";
 
+my $stillbirths_file      = 'raw_data/VSB357005_20240222_014547_21.csv';
 my $oia_deaths_file       = 'raw_data/25021_Data_Attachment_1.csv';                                          # from https://fyi.org.nz/request/25021-number-of-covid19-vax-deaths-by-age-band-location-and-month#incoming-96520
 my $deaths_by_months_file = 'raw_data/Monthly_death_registrations_by_ethnicity_age_sex_Jan2010_Sep2023.csv'; # from https://www.stats.govt.nz/assets/Uploads/Births-and-deaths/Births-and-deaths-Year-ended-September-2023/Download-data/Monthly-death-registrations-by-ethnicity-age-sex-Jan2010-Sep2023.xlsx
 
 my %oia_deaths            = ();
 my %deaths_by_months      = ();
 
+my %under_20_deaths       = ();
+my %oia_0_20_deaths       = ();
+
 my $default_under_5       = 2;
 
 load_deaths();
 load_oia_deaths();
 print_compare();
+my %yearly_mort_under_20  = ();
+print_0_20_oia_to_0_19_ref_compare();
+load_stillbirths();
+print_2020_2022_stillbirths_included_compare();
 
 sub load_deaths {
 	say "Loading deaths ...";
@@ -41,6 +49,14 @@ sub load_deaths {
 		next if $year eq 'year_reg';
 		next unless $ethnicity eq 'Total';
 		$deaths_by_months{$year}->{$month} += $count;
+		# say "age_group : $age_group";
+		if ($age_group eq '00_00' ||
+			$age_group eq '01_04' ||
+			$age_group eq '05_09' ||
+			$age_group eq '10_14' ||
+			$age_group eq '15_19') {
+			$under_20_deaths{$year}->{$month} += $count;
+		}
 	}
 	close $in;
 }
@@ -53,13 +69,15 @@ sub load_oia_deaths {
 		my ($year_month, $age_group, $last_dose, $days_to_death, $count) = split ',', $_;
 		next if $year_month eq 'Month of Death';
 		next if $year_month eq 'Total';
-		# say $_;
 		if ($count eq '<5') {
 			$count = $default_under_5;
 		}
 		my ($year, $month) = split '-', $year_month;
 		if ($month < 10) {
 			$month =~ s/0//;
+		}
+		if ($age_group eq '0 to 20') {
+			$oia_0_20_deaths{$year}->{$month} += $count;
 		}
 		$oia_deaths{$year}->{$month} += $count;
 	}
@@ -84,4 +102,43 @@ sub print_compare {
 	my $oia_minus_ref = $oia_total - $ref_total;
 	close $out;
 	say "oia_minus_ref : $oia_minus_ref";
+}
+
+sub print_0_20_oia_to_0_19_ref_compare {
+	open my $out, '>:utf8', 'data/under_20_deaths_compare.csv';
+	say $out "year_month,OIA Total,Ref. Total";
+	for my $year (sort{$a <=> $b} keys %oia_0_20_deaths) {
+		for my $month (sort{$a <=> $b} keys %{$oia_0_20_deaths{$year}}) {
+			my $oia_count = $oia_0_20_deaths{$year}->{$month} // die;
+			my $ref_count = $under_20_deaths{$year}->{$month}  // next;
+			$yearly_mort_under_20{$year}->{'ref'} += $ref_count;
+			$yearly_mort_under_20{$year}->{'oia'} += $oia_count;
+			say $out "$year-$month,$oia_count,$ref_count";
+		}
+	}
+	close $out;
+}
+
+sub load_stillbirths {
+	my %headers = ();
+	open my $in, '<:utf8', $stillbirths_file;
+	while (<$in>) {
+		chomp $_;
+		$_ =~ s/\"//g;
+		my ($year, $value) = split ',', $_;
+		next unless defined $year && looks_like_number $year;
+		$yearly_mort_under_20{$year}->{'ref'} += $value;
+	}
+	close $in;
+}
+
+sub print_2020_2022_stillbirths_included_compare {
+	open my $out, '>:utf8', 'data/under_20_deaths_and_stillbirths_compare.csv';
+	say $out "Year,OIA Total,Ref. Total";
+	for my $year (2020 .. 2022) {
+		my $ref_count = $yearly_mort_under_20{$year}->{'ref'} // die;
+		my $oia_count = $yearly_mort_under_20{$year}->{'oia'} // die;
+		say $out "$year,$oia_count,$ref_count";
+	}
+	close $out;
 }
